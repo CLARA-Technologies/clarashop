3348e4811660379408c3f4b33ec9152b5e58317f
singh <singh@192.168.0.7>
2020-04-15 20:19:16 +0530

    Complete the interface , themes and dynamic menu title, active menu.

--- src/main/webapp/assets/js/myapp.js
+++ src/main/webapp/assets/js/myapp.js
@@ -1,7 +1,7 @@
-$(function(){ 
-	
+$(function() {
 	// solving the active menu problem
-	switch(menu){
+	switch (menu) {
+
 	case 'About Us':
 		$('#about').addClass('active');
 		break;
@@ -12,4 +12,5 @@ $(function(){
 		$('#home').addClass('active');
 		break;
 	}
+
 });
